a1ea00e6653eedb4bededd3bb5fcef65300d1cd0
asaei623 <himtows623@gmail.com>
2022-09-02 19:03:15 +0900

    백 연결 테스트용 코드 추가

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -7,232 +7,246 @@ import SigninPage from "./pages/signin";
 import SignUpPage from "./pages/signup";
 import MyPage from "./components/MyInfo";
 import FindIdPw from "./components/forgotIdPw";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Main from "./pages/Main";
 import SearchChallenges from "./pages/SearchChallenges";
 import Challenge from "./pages/Challenge";
+import axios from "axios";
 //import { Nav } from "./components/Navbar/NavbarElements";
 
 function App() {
-  /*data*/
-  const userData = {
-    username: "Timothee Chalamet",
-    color: ["#ffdce0", "#f5f0ff", "#dcffe4", "#fff5b1"],
-    nickname: "peach",
-    email: "Timo22@google.com",
-    userId: "Timooo24",
-    userpw: "ennlab22",
-    checkPW: "ennlab22"
-  };
-  const challengeData = [
-    {
-      clgid: "1111",
-      clgname: "운동1",
-      clgmanagerid: "ccc1",
-      clginfo: "강의 2개씩 듣고 인증",
-      clgmemberno: 10,
-      clgCateId: "운동",
-      made_date: "2022-08-20",
-      end_date: "2024-11-11"
-    },
-    {
-      clgid: "2222",
-      clgname: "운동2",
-      clgmanagerid: "ccc1",
-      clginfo: "강의 2개씩 듣고 인증",
-      clgmemberno: 10,
-      clgCateId: "운동",
-      made_date: "2022-08-20",
-      end_date: "2024-11-11"
-    },
-    {
-      clgid: "3333",
-      clgname: "운동3",
-      clgmanagerid: "ccc1",
-      clginfo: "강의 2개씩 듣고 인증",
-      clgmemberno: 10,
-      clgCateId: "운동",
-      made_date: "2022-08-20",
-      end_date: "2024-11-11"
-    },
-    {
-      clgid: "1",
-      clgname: "공부1",
-      clgmanagerid: "ccc1",
-      clginfo: "강의 2개씩 듣고 인증",
-      clgmemberno: 10,
-      clgCateId: "공부",
-      made_date: "2022-04-20",
-      end_date: "2024-02-11"
-    },
-    {
-      clgid: "2",
-      clgname: "공부2",
-      clgmanagerid: "ccc1",
-      clginfo: "강의 2개씩 듣고 인증",
-      clgmemberno: 10,
-      clgCateId: "공부",
-      made_date: "2022-04-20",
-      end_date: "2024-02-11"
-    },
-    {
-      clgid: "3",
-      clgname: "공부3",
-      clgmanagerid: "ccc1",
-      clginfo: "강의 2개씩 듣고 인증",
-      clgmemberno: 10,
-      clgCateId: "공부",
-      made_date: "2022-04-20",
-      end_date: "2024-02-11"
-    },
-    {
-      clgid: "4",
-      clgname: "공부4",
-      clgmanagerid: "ccc1",
-      clginfo: "강의 2개씩 듣고 인증",
-      clgmemberno: 10,
-      clgCateId: "공부",
-      made_date: "2022-04-20",
-      end_date: "2024-02-11"
-    },
-    {
-      clgid: "5",
-      clgname: "공부5",
-      clgmanagerid: "ccc1",
-      clginfo: "강의 2개씩 듣고 인증",
-      clgmemberno: 10,
-      clgCateId: "공부",
-      made_date: "2022-04-20",
-      end_date: "2024-02-11"
-    }
-  ];
-  const userChallengeData = [
-    {
-      clgRelNo: 1,
-      clgDetail: {
-        clgName: "30분 독서",
-        clgColor: "#ffdce0",
-        clgComplete: true
-      }
-    },
-    {
-      clgRelNo: 2,
-      clgDetail: {
-        clgName: "일기쓰기",
-        clgColor: "#dcffe4",
-        clgComplete: false
-      }
-    },
-    {
-      clgRelNo: 3,
-      clgDetail: {
-        clgName: "운동하기",
-        clgColor: "#f5f0ff",
-        clgComplete: true
-      }
-    },
-    {
-      clgRelNo: 4,
-      clgDetail: {
-        clgName: "30분 스트레칭",
-        clgColor: "#fff5b1",
-        clgComplete: true
-      }
-    }
-  ];
-  const [data, setData] = useState(userChallengeData);
-  const onEditColor = (targetId, newColor) => {
-    setData(
-      data.map((it) =>
-        it.clgRelNo === targetId
-          ? {
-              ...it,
-              clgDetail: { ...it.clgDetail, clgColor: newColor }
-            }
-          : it
-      )
-    );
-  };
-  const onEditComplete = (targetId) => {
-    setData(
-      data.map((it) =>
-        it.clgRelNo === targetId
-          ? {
-              ...it,
-              clgDetail: {
-                ...it.clgDetail,
-                clgComplete: it.clgDetail.clgComplete ? false : true
-              }
-            }
-          : it
-      )
-    );
-  };
-  const onDelete = (targetId) => {
-    setData(data.filter((it) => it.clgRelNo !== targetId));
-    console.log(data);
-  };
-  const [clgs, setClgs] = useState(challengeData);
-  const showCategoryClgs = (categoryName) => {
-    setClgs(challengeData.filter((it) => it.clgCateId === categoryName));
-  };
-  const showFilteredClgs = (filtered) => {
-    const result = [];
-    filtered.map((f) =>
-      result.push(challengeData.filter((it) => it.clgid === f.clgid))
-    );
-    for (let i = 0; i < result.length; i++) {
-      result[i] = result[i][0];
-    }
-    setClgs(result);
-  };
-  const [clg, setClg] = useState();
-  const setClickedClg = (it) => {
-    setClg(it);
-  };
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} exact />
-        <Route path="/signin" element={<SigninPage />} exact />
-        <Route path="/signup" element={<SignUpPage />} exact />
-        <Route path="/MyInfo" element={<MyPage />} exact />
-        <Route path="/forgotIdPw" element={<FindIdPw />} exact />
+    /*백엔드 연결*/
+    const [test, setTest] = useState("");
 
-        <Route
-          path="/main"
-          element={
-            <Main
-              onEditComplete={onEditComplete}
-              challengeData={challengeData}
-              userChallengeData={data}
-              onEditColor={onEditColor}
-              onDelete={onDelete}
-              userData={userData}
-              showCategoryClgs={showCategoryClgs}
-              showFilteredClgs={showFilteredClgs}
-            />
-          }
-        ></Route>
-        <Route
-          path="/searchChallenges"
-          element={
-            <SearchChallenges
-              challengeData={challengeData}
-              showCategoryClgs={showCategoryClgs}
-              showFilteredClgs={showFilteredClgs}
-              clgs={clgs}
-              setClickedClg={setClickedClg}
-            />
-          }
-        ></Route>
-        <Route
-          path="/challenge"
-          element={<Challenge clg={clg}></Challenge>}
-        ></Route>
-      </Routes>
-    </Router>
-  );
+    useEffect(() => {
+        axios
+            .get("/api/users/signup")
+            .then((response) => setTest(response.data))
+            .catch((error) => console.log(error));
+    }, []);
+
+    /*data*/
+    const userData = {
+        username: "Timothee Chalamet",
+        color: ["#ffdce0", "#f5f0ff", "#dcffe4", "#fff5b1"],
+        nickname: "peach",
+        email: "Timo22@google.com",
+        userId: "Timooo24",
+        userpw: "ennlab22",
+        checkPW: "ennlab22",
+    };
+    const challengeData = [
+        {
+            clgid: "1111",
+            clgname: "운동1",
+            clgmanagerid: "ccc1",
+            clginfo: "강의 2개씩 듣고 인증",
+            clgmemberno: 10,
+            clgCateId: "운동",
+            made_date: "2022-08-20",
+            end_date: "2024-11-11",
+        },
+        {
+            clgid: "2222",
+            clgname: "운동2",
+            clgmanagerid: "ccc1",
+            clginfo: "강의 2개씩 듣고 인증",
+            clgmemberno: 10,
+            clgCateId: "운동",
+            made_date: "2022-08-20",
+            end_date: "2024-11-11",
+        },
+        {
+            clgid: "3333",
+            clgname: "운동3",
+            clgmanagerid: "ccc1",
+            clginfo: "강의 2개씩 듣고 인증",
+            clgmemberno: 10,
+            clgCateId: "운동",
+            made_date: "2022-08-20",
+            end_date: "2024-11-11",
+        },
+        {
+            clgid: "1",
+            clgname: "공부1",
+            clgmanagerid: "ccc1",
+            clginfo: "강의 2개씩 듣고 인증",
+            clgmemberno: 10,
+            clgCateId: "공부",
+            made_date: "2022-04-20",
+            end_date: "2024-02-11",
+        },
+        {
+            clgid: "2",
+            clgname: "공부2",
+            clgmanagerid: "ccc1",
+            clginfo: "강의 2개씩 듣고 인증",
+            clgmemberno: 10,
+            clgCateId: "공부",
+            made_date: "2022-04-20",
+            end_date: "2024-02-11",
+        },
+        {
+            clgid: "3",
+            clgname: "공부3",
+            clgmanagerid: "ccc1",
+            clginfo: "강의 2개씩 듣고 인증",
+            clgmemberno: 10,
+            clgCateId: "공부",
+            made_date: "2022-04-20",
+            end_date: "2024-02-11",
+        },
+        {
+            clgid: "4",
+            clgname: "공부4",
+            clgmanagerid: "ccc1",
+            clginfo: "강의 2개씩 듣고 인증",
+            clgmemberno: 10,
+            clgCateId: "공부",
+            made_date: "2022-04-20",
+            end_date: "2024-02-11",
+        },
+        {
+            clgid: "5",
+            clgname: "공부5",
+            clgmanagerid: "ccc1",
+            clginfo: "강의 2개씩 듣고 인증",
+            clgmemberno: 10,
+            clgCateId: "공부",
+            made_date: "2022-04-20",
+            end_date: "2024-02-11",
+        },
+    ];
+    const userChallengeData = [
+        {
+            clgRelNo: 1,
+            clgDetail: {
+                clgName: "30분 독서",
+                clgColor: "#ffdce0",
+                clgComplete: true,
+            },
+        },
+        {
+            clgRelNo: 2,
+            clgDetail: {
+                clgName: "일기쓰기",
+                clgColor: "#dcffe4",
+                clgComplete: false,
+            },
+        },
+        {
+            clgRelNo: 3,
+            clgDetail: {
+                clgName: "운동하기",
+                clgColor: "#f5f0ff",
+                clgComplete: true,
+            },
+        },
+        {
+            clgRelNo: 4,
+            clgDetail: {
+                clgName: "30분 스트레칭",
+                clgColor: "#fff5b1",
+                clgComplete: true,
+            },
+        },
+    ];
+    const [data, setData] = useState(userChallengeData);
+    const onEditColor = (targetId, newColor) => {
+        setData(
+            data.map((it) =>
+                it.clgRelNo === targetId
+                    ? {
+                          ...it,
+                          clgDetail: { ...it.clgDetail, clgColor: newColor },
+                      }
+                    : it
+            )
+        );
+    };
+    const onEditComplete = (targetId) => {
+        setData(
+            data.map((it) =>
+                it.clgRelNo === targetId
+                    ? {
+                          ...it,
+                          clgDetail: {
+                              ...it.clgDetail,
+                              clgComplete: it.clgDetail.clgComplete
+                                  ? false
+                                  : true,
+                          },
+                      }
+                    : it
+            )
+        );
+    };
+    const onDelete = (targetId) => {
+        setData(data.filter((it) => it.clgRelNo !== targetId));
+        console.log(data);
+    };
+    const [clgs, setClgs] = useState(challengeData);
+    const showCategoryClgs = (categoryName) => {
+        setClgs(challengeData.filter((it) => it.clgCateId === categoryName));
+    };
+    const showFilteredClgs = (filtered) => {
+        const result = [];
+        filtered.map((f) =>
+            result.push(challengeData.filter((it) => it.clgid === f.clgid))
+        );
+        for (let i = 0; i < result.length; i++) {
+            result[i] = result[i][0];
+        }
+        setClgs(result);
+    };
+    const [clg, setClg] = useState();
+    const setClickedClg = (it) => {
+        setClg(it);
+    };
+    return (
+        <Router>
+            <div>백엔드에서 가져온 데이터입니다 : {test}</div>
+            <Routes>
+                <Route path="/" element={<Home />} exact />
+                <Route path="/signin" element={<SigninPage />} exact />
+                <Route path="/signup" element={<SignUpPage />} exact />
+                <Route path="/MyInfo" element={<MyPage />} exact />
+                <Route path="/forgotIdPw" element={<FindIdPw />} exact />
+
+                <Route
+                    path="/main"
+                    element={
+                        <Main
+                            onEditComplete={onEditComplete}
+                            challengeData={challengeData}
+                            userChallengeData={data}
+                            onEditColor={onEditColor}
+                            onDelete={onDelete}
+                            userData={userData}
+                            showCategoryClgs={showCategoryClgs}
+                            showFilteredClgs={showFilteredClgs}
+                        />
+                    }
+                ></Route>
+                <Route
+                    path="/searchChallenges"
+                    element={
+                        <SearchChallenges
+                            challengeData={challengeData}
+                            showCategoryClgs={showCategoryClgs}
+                            showFilteredClgs={showFilteredClgs}
+                            clgs={clgs}
+                            setClickedClg={setClickedClg}
+                        />
+                    }
+                ></Route>
+                <Route
+                    path="/challenge"
+                    element={<Challenge clg={clg}></Challenge>}
+                ></Route>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
